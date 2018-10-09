86f2c653b90012006c462275e3d6083a93f462dd
larryzodiac <09kevinsevanmachale@gmail.com>
2018-10-09 17:25:03 +0100

    Colour Spectrum Edit

--- 01_colour/01.1_colour_spectrum/01.1.1_colour_spectrum_grid/sketch.js
+++ 01_colour/01.1_colour_spectrum/01.1.1_colour_spectrum_grid/sketch.js
@@ -2,31 +2,49 @@
 // 17/09/2018
 // Evan MacHale - N00150552
 // ------------------------------------------------- //
+'use strict';
+
+let canvasSize;
+
+// let stepX;
+// let stepY;
 
 function setup() {
-//    createCanvas(500,500);
-    createCanvas(windowWidth,windowHeight);
-    colorMode(HSB, windowWidth, 100, 100);
+  canvasSize = $('#canvas').width();
+  let canvas = createCanvas(canvasSize,canvasSize);
+  canvas.parent('canvas');
+  colorMode(HSB, width, height, 100);
+  noStroke();
 }
 
 function draw() {
-//    fill(360, 100, 100);
-    noStroke();
-    var numOfCols = 100;
-    var numOfRows = 100;
-    var stepX = windowWidth/numOfCols;
-    var stepY = windowHeight/numOfRows;
-    
-    for(var gridY = 0; gridY < windowHeight; gridY += stepY) {
-        for(var gridX = 0; gridX < windowWidth; gridX += stepX) {
-            fill(gridX, 100, 100);
-            rect(gridX, gridY, stepX, stepY);
-        }
+  var numOfCols = 100;
+  var numOfRows = 100;
+  var stepX = width/numOfCols;
+  var stepY = height/numOfRows;
+
+  for(var gridY = 0; gridY < height; gridY += stepY) {
+    for(var gridX = 0; gridX < width; gridX += stepX) {
+      fill(gridX, 100, 100);
+      rect(gridX, gridY, stepX, stepY);
     }
+  }
 }
 
+// function draw() {
+//   stepX = mouseX + 20;
+//   stepY = mouseY + 20;
+//
+//   for (var gridY = 0; gridY < height; gridY += stepY) {
+//     for (var gridX = 0; gridX < width; gridX += stepX) {
+//       fill(gridX, height - gridY, 100);
+//       rect(gridX, gridY, stepX, stepY);
+//     }
+//   }
+// }
+
 function keyPressed(){
-    if(key==='s' || key==='S'){
-        saveCanvas(gd.timestamp() + '_mouseX_' + mouseX, 'png');
-    }
+  if(key==='s' || key==='S'){
+    saveCanvas(gd.timestamp() + '_mouseX_' + mouseX, 'png');
+  }
 }
