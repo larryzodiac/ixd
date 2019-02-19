81b53029c9d84a347c0e1ed65706d2c58fe75ad0
larryzodiac <evanmachale@gmail.com>
2019-02-19 20:17:37 +0000

    02_assignment > 02.0

--- 02_assignment/02.0/material.js
+++ 02_assignment/02.0/material.js
@@ -5,7 +5,15 @@
 // https://github.com/material-components/material-components-web
 // ------------------------------------------------- //
 
-// slider
+// Global
 const { MDCSlider } = mdc.slider;
-const font_slider = new mdc.slider.MDCSlider(document.querySelector('.mdc-slider'));
-font_slider.listen('MDCSlider:change', () => console.log(`Value changed to ${slider.value}`));
+
+// font slider
+const font_slider = new mdc.slider.MDCSlider(document.querySelector('.font-slider'));
+// font_slider.listen('MDCSlider:change', () => console.log(`Value changed to ${slider.value}`));
+
+// point density slider
+const point_density_slider = new mdc.slider.MDCSlider(document.querySelector('.point-density-slider'));
+
+// shape size slider
+const shape_size_slider = new mdc.slider.MDCSlider(document.querySelector('.shape_size_slider'));
--- 02_assignment/02.0/sketch.js
+++ 02_assignment/02.0/sketch.js
@@ -19,11 +19,14 @@
 let canvas;
 
 let plex;
-let font_size = 500;
-let typed_text = 'p5.js';
-let image;
+let font_size;
+let typed_text = 'Gestaltung';
+let text_graphic;
+let shape_size;
 
-let point_density = 10;
+let point_density;
+
+// ------------------------------------------------- //
 
 // The seed that will spawn our p5 sketch.
 const seed = (sketch) => {
@@ -32,50 +35,77 @@ const seed = (sketch) => {
     plex = sketch.loadFont('../../assets/IBMPlexSans-Regular.otf');
   }
 
+  // ------------------------------------------------- //
+
   sketch.setup = () => {
     // canvas_size can be decimals, pixel array doesn't like it. i.e can't have pixel 0.5
     let canvas_width = sketch.floor($('#canvas').width());
-    // Sizing + appending the canvas to a div.
     canvas = sketch.createCanvas(canvas_width, sketch.windowHeight);
-    // canvas.background(255);
     canvas.parent('canvas');
+
     // Exercise variables
-    // Slider from 'material.js'
+    text_graphic = sketch.createGraphics(sketch.width, sketch.height);
+
+    // Sliders from 'material.js'
+    // font size
     font_slider.min = 100;
     font_slider.max = 600;
-    font_slider.value = 200;
+    font_slider.value = 250;
+    // point density
+    point_density_slider.min = 1;
+    point_density_slider.max = 50;
+    point_density_slider.value = 4;
+    // point density
+    shape_size_slider.min = 1;
+    shape_size_slider.max = 30;
+    shape_size_slider.value = 2;
+
+    // Defaults
+    sketch.fill(0);
+    sketch.noStroke();
   }
 
+  // ------------------------------------------------- //
+
   sketch.draw = () => {
-    canvas.background(0);
-    sketch.fill(255);
-    sketch.noStroke();
+    sketch.background(255);
+    draw_graphic();
 
-    setup_text();
-    font_size = font_slider.value
+    font_size = font_slider.value;
+    point_density = point_density_slider.value;
+    shape_size = shape_size_slider.value;
 
-    for (let y = 0; y < image.height; y += point_density) {
-      for (let x = 0; x < image.width; x += point_density) {
+    for (let y = 0; y < text_graphic.height; y += point_density) {
+      for (let x = 0; x < text_graphic.width; x += point_density) {
         // Calculate the index for the pixels array from x and y
-        let index = (x + y * image.width) * 4;
-        let r = image.pixels[index];
+        let index = (x + y * text_graphic.width) * 4;
+        let r = text_graphic.pixels[index];
 
         if (r < 128) {
-          sketch.ellipse(x, y, 5, 5);
+          sketch.ellipse(x, y, shape_size);
         }
       }
     }
   }
 
-  const setup_text = () => {
-    image = sketch.createGraphics(sketch.width, sketch.height);
-    image.pixelDensity(1);
-    image.background(255);
-    image.textFont(plex);
-    image.textSize(font_size);
-    image.textAlign(sketch.CENTER, sketch.CENTER);
-    image.text(typed_text, sketch.width/2, sketch.height/3);
-    image.loadPixels();
+  // ------------------------------------------------- //
+
+  const draw_graphic = () => {
+    text_graphic.pixelDensity(1);
+    text_graphic.background(255);
+    text_graphic.textFont(plex);
+    text_graphic.textSize(font_size);
+    text_graphic.textAlign(sketch.CENTER, sketch.CENTER);
+    text_graphic.text(typed_text, sketch.width/2, sketch.height/2.5);
+    text_graphic.loadPixels();
+  }
+
+  // ------------------------------------------------- //
+
+  sketch.windowResized = () => {
+    let canvas_width = sketch.floor($('#canvas').width());
+    sketch.resizeCanvas(canvas_width, sketch.windowHeight);
+    draw_graphic();
   }
 
   // Saving meaningful parametre data.
@@ -88,7 +118,7 @@ const seed = (sketch) => {
     if (sketch.keyCode == sketch.DELETE || sketch.keyCode == sketch.BACKSPACE) {
       if (typed_text.length > 0) {
         typed_text = typed_text.substring(0, typed_text.length - 1);
-        setup_text();
+        draw_graphic();
       }
     }
   }
@@ -96,9 +126,12 @@ const seed = (sketch) => {
   sketch.keyTyped = () => {
     if (sketch.keyCode >= 32) {
       typed_text += sketch.key;
-      setup_text();
+      draw_graphic();
     }
   }
+
+  // ------------------------------------------------- //
+
 }
 
 // Create p5 instance w/ new p5 constructor + specify parent for elements.
