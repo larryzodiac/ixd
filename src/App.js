6b297859e2b0f07e6dd88ba4aa8196557ff00a43
Glitch (larryzodiac-creative-coding-deleted-1) <none>
2019-03-25 12:39:38 +0000

    🗻🎑 Updated with Glitch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello App
+      <div class="mdc-layout-grid">
+        <div class="mdc-layout-grid__inner">
+          <div class="mdc-layout-grid__cell">Hi</div>
+          <div class="mdc-layout-grid__cell">Wow</div>
+          <div class="mdc-layout-grid__cell">Geesuz</div>
+        </div>
       </div>
     );
   }
